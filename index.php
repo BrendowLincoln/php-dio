87ddd860f8dc7dc85fbfb887070b62b7ddf70b3f
BrendowLincoln <linconlbrendow@gmail.com>
2020-10-24 15:13:48 -0300

    Capitando dados  do formulário

--- index.php
+++ index.php
@@ -3,15 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulário</title>
+    <title>Formulário de inscrição</title>
 </head>
 <body>
 
-    <?php
+    <p>FORMULÁIO DE INSCRIÇÃO DE COMPETIDORES</p>
 
-        echo "Hello World!";
 
-    ?>
+    <form action="script.php" method="post">
+        <p>Seu nome: <input type="text" name="nome" /></p>
+        <p>Sua idade: <input type="text" name="idade" /></p>
+        <p><input type="submit" value="Enviar dados do competidor"/></p>
+    </form>
     
 </body>
 </html>
